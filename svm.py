6e4969c123ea8d739a1741739704e76013e9ef82
JacksonTrust7 <66334818+JacksonTrust7@users.noreply.github.com>
2020-12-09 18:42:03 -0700

    final commit for svm

--- svm.py
+++ svm.py
@@ -1,13 +1,10 @@
 import pandas as pd
-import numpy as np
 import texthero as hero
-import timeit
-from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.model_selection import train_test_split , GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, precision_score, recall_score
-import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
+from sklearn.pipeline import make_pipeline, Pipeline
 
 def clean_dataframe(df):
 	df['clean_reviewText'] = hero.clean(df['reviewText'])
@@ -23,34 +20,35 @@ def train_svm(df):
     x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
     print("Word count complete")
     
-    clf = SVC()
+    param_grid = {'svc__C': [0.3, 0.7, 1.1], 'svc__gamma': [0.1,0.3,0.5], 'svc__kernel': ['rbf']}
+    pipeline = make_pipeline(count_vect, SVC())
+    grid = GridSearchCV(pipeline, param_grid, n_jobs=-1, verbose=2)
+    grid.fit(x_train, y_train)
+    print("Parameter tuning complete")
+    print("Best parameters : ", grid.best_params_)
     
-    start = timeit.default_timer()
+    clf = SVC(C = grid.best_params_["svc__C"], gamma = grid.best_params_["svc__gamma"])
     clf.fit(x_train_tfidf, y_train)
-    stop = timeit.default_timer()
-    print("Model fitting complete, runtime = ",stop - start," seconds")
-    
     x_test_transform = count_vect.transform(x_test)
     y_pred = clf.predict(x_test_transform)
-    print("Model testing complete, metrics incoming")
-    
+    print("Model fitting/testing complete, metrics incoming")
     print("Confusion matrix : ", confusion_matrix(y_test,y_pred))
-    print("Precision Score : ", precision_score(y_test,y_pred, average='micro'))
-    print("Recall Score :" , recall_score(y_test, y_pred, average='micro') )
+    print("Precision Score : ", precision_score(y_test,y_pred, average='weighted'))
+    print("Recall Score :" , recall_score(y_test, y_pred, average='weighted'))
+    print("Accuracy Score : ", accuracy_score(y_test, y_pred))
+    print("F1 Score : ", f1_score(y_test, y_pred, average='weighted'))
     
 
 def main():
     print("Imports successful")
     
-    mrdf = pd.read_json("Digital_Music_5.json", lines=True)
+    mrdf = pd.read_json("Pet_Supplies_5.json", lines=True)
+    mrdf = mrdf.sample(frac = 0.02)
+    print("Dataframe length:", len(mrdf))
+    print("Dataset shrunk to 2% of original size")
+    
     mrdf = clean_dataframe(mrdf)
     print("Dataframe cleaned")
-    #print(mrdf.reviewText[0])
-    #print(mrdf.clean_reviewText[0])
-    
-    mrdf = mrdf.sample(frac = 0.3)
-    print("Dataframe length:", len(mrdf))
-    print("Dataset shrunk to 30% of original size")
     
     train_svm(mrdf)
     
